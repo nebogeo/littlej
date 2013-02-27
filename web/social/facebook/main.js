ab539594a4da7eb7d8fe506e7d3cac9b3c45584e
Dave Griffiths <dave@fo.am>
2013-02-27 09:16:26 +0000

    added fb login

--- web/social/facebook/main.js
+++ web/social/facebook/main.js
@@ -1,10 +1,50 @@
 
 
+function fb_interface(appid)
+{   
+    if (appid!="") 
+    {
+        $(document).ready(function() {
+	        FB.init({appId: appid, status: true, cookie: true, xfbml: true});
+	    });
+    }
+    
+    this.accessToken=false;
+    this.uid=false;
+
+    this.login=function()
+    {
+        var fb=this;
+        
+        FB.getLoginStatus(function(response) {
+            if (response.status=="connected") {
+                console.log("logged in already...");
+		        fb.uid = response.authResponse.userID;
+		        fb.accessToken = response.authResponse.accessToken;
+	        }
+	        else
+	        {
+		        FB.login(function(response) {
+                    console.log("logging in...");
+			        if (response.authResponse) {
+			            fb.accessToken = response.authResponse.accessToken;
+			        }
+		        }, {
+                    scope:'user_about_me'
+                });
+	        }		
+	    });
+    }
+}
+
 $.ajaxSetup({
     error: function(xhr, status, error) {
         alert("An AJAX error occured: " + status + "\nError: " + error);
     }
 });
+
+var fb = new fb_interface(api_key);
+fb.login(); // should be logged in already
 
 function send_to(url) {    
     
