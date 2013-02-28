57e57898c4ae36851e24fc78d853cae9b23fa50a
Dave Griffiths <dave@fo.am>
2013-02-28 10:15:16 +0000

    rejig

--- web/social/facebook/main.js
+++ web/social/facebook/main.js
@@ -152,6 +152,7 @@ var fb = new fb_interface(api_key);
 function send_to(url) {       
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (pos) {
+            alert(pos);
             alert(JSON.stringify(pos));
             send(pos);
         });
