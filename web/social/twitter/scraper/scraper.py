eab8bbfb5c634d6aaef677705cf39bc8d2d7a1a1
Dave Griffiths <dave@fo.am>
2013-03-03 17:28:22 +0000

    twitter scanner semi functional

--- web/social/twitter/scraper/scraper.py
+++ web/social/twitter/scraper/scraper.py
@@ -1,12 +1,58 @@
 import urllib
 import simplejson
+import datetime
+import dateutil.parser as parser
+import time
 
-def searchTweets(query):
- search = urllib.urlopen("http://search.twitter.com/search.json?q="+query)
- dict = simplejson.loads(search.read())
- for result in dict["results"]: # result is a list of dictionaries
-  print "*",result["text"],"\n"
 
-# we will search tweets about "fc liverpool" football team
-searchTweets("fc+liverpool")
+def search_tweets(last,query):
+    search = urllib.urlopen("http://search.twitter.com/search.json?q="+query)
+    dict = simplejson.loads(search.read())
+    latest=last
+    for tweet in dict["results"]: # result is a list of dictionaries
+        time = parser.parse(tweet["created_at"])
+        if time>last:
+            #if tweet["geo"]!=None and tweet["geo"]["type"]=='Point':
+            send(tweet,time)
+            print tweet
+            latest=time
+    return latest
 
+
+def param(a,b):
+    return "?"+a+"="+b;
+
+def send(tweet,time):
+    ampm="pm"
+    if time.hour<12: ampm="am"
+    latlon=[0,0]
+    if tweet["geo"]!=None and tweet["geo"]["type"]=='Point':
+        latlon=tweet["geo"]["coordinates"]
+
+    url="localhost/api"+param("task", "report")+\
+        param("incident_title", tweet["text"])+\
+        param("incident_description", "From Twitter")+\
+        param("incident_date", str(time.month)+"/"+str(time.day)+"/"+str(time.year))+\
+        param("incident_hour", str(time.hour))+\
+        param("incident_minute", str(time.minute))+\
+        param("incident_ampm", ampm)+\
+        param("incident_category", "1")+\
+        param("latitude", str(latlon[0]))+\
+        param("longitude", str(latlon[1]))+\
+        param("location_name", "No location")+\
+        param("person_first", tweet["from_user_name"])+\
+        param("person_last", "");
+
+    print url
+
+    urllib.urlopen(url,"POST")
+
+
+def main():
+    last=parser.parse("2013-01-01 12:00:00 +0000");
+    while 1:
+        last=search_tweets(last,"#littlejporttalbot")
+        time.sleep(2)
+    
+
+main()
