9fd8d6f230c00dd1f0e0f462cc14cd8c5a74766c
Dave Griffiths <dave@fo.am>
2013-03-16 10:28:43 +0000

    removed survey

--- web/themes/default/views/reports/submit_thanks.php
+++ web/themes/default/views/reports/submit_thanks.php
@@ -9,7 +9,7 @@
 
 				<div class="thanks_msg"><a href="<?php echo
 					url::site().'reports' ?>"><?php echo Kohana::lang('ui_main.reports_return');?></a><br /><br /><br />
-					<?php echo Kohana::lang('ui_main.feedback_reports');?><br /><br />
+<!--					<?php echo Kohana::lang('ui_main.feedback_reports');?><br /><br />
 					<?php 
 					print form::open('http://feedback.ushahidi.com/fillsurvey.php?sid=2', array('target'=>'_blank'));
 					print form::hidden('alert_code', $_SERVER['SERVER_NAME']);
@@ -17,6 +17,7 @@
 					print form::submit('button', Kohana::lang('ui_main.feedback'), ' class=btn_gray ');
 					print form::close();
 					?>
+-->
 				</div>
 			</div>
 		</div>
