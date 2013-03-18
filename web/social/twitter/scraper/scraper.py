385b5e9800f7427514c73aa14f81a8516c0985e5
dave griffiths <dave@fo.am>
2013-03-18 15:16:33 +0000

    twitter: searching for any geotagged port talbot tweets

--- web/social/twitter/scraper/scraper.py
+++ web/social/twitter/scraper/scraper.py
@@ -37,39 +37,41 @@ def search_tweets(last,query):
 
 
 def send(tweet,time):
-    print tweet["text"]
-    ampm="pm"
-    if time.hour<12: ampm="am"
-    latlon=[0,0]
-    if tweet["geo"]!=None and tweet["geo"]["type"]=='Point':
-        latlon=tweet["geo"]["coordinates"]
-    date="%02d"%time.month+"/"+"%02d"%time.day+"/"+str(time.year)
-    url="http://littlej.borrowed-scenery.com/api"
-    data=urllib.urlencode({"task":"report",
-                           "incident_title": tweet["text"].encode("ascii","ignore"),
-                           "incident_description": "From Twitter",
-                           "incident_date": date,
-                           "incident_hour": time.hour%12,
-                           "incident_minute": time.minute,
-                           "incident_ampm": ampm,
-                           "incident_category": "1",
-                           "latitude": str(latlon[0]),
-                           "longitude": str(latlon[1]),
-                           "location_name": "No location",
-                           "person_first": tweet["from_user_name"],
-                           "person_last": ""});
-#    print url
-#    print data
-    sock = urllib.urlopen(url,data)
-    print sock.read()
-    sock.close()
+    ## only if geotagged with a point
+    if tweet["geo"]!=None and tweet["geo"]["type"]=='Point' and tweet["geo"]["coordinates"]!=None:
+        print tweet["text"]
+        ampm="pm"
+        if time.hour<12: ampm="am"
+        latlon=[0,0]
+        if tweet["geo"]!=None and tweet["geo"]["type"]=='Point':
+            latlon=tweet["geo"]["coordinates"]
+            date="%02d"%time.month+"/"+"%02d"%time.day+"/"+str(time.year)
+            url="http://littlej.borrowed-scenery.com/api"
+            data=urllib.urlencode({"task":"report",
+                                   "incident_title": tweet["text"].encode("ascii","ignore"),
+                                   "incident_description": "From Twitter",
+                                   "incident_date": date,
+                                   "incident_hour": time.hour%12,
+                                   "incident_minute": time.minute,
+                                   "incident_ampm": ampm,
+                                   "incident_category": "1",
+                                   "latitude": str(latlon[0]),
+                                   "longitude": str(latlon[1]),
+                                   "location_name": "No location",
+                                   "person_first": tweet["from_user_name"],
+                                   "person_last": ""});
+            #    print url
+            #    print data
+            sock = urllib.urlopen(url,data)
+            print sock.read()
+            sock.close()
 
 
 def main():
 #    last=parser.parse("2013-01-01 12:00:00 +0000");
     last=load_latest();
     while 1:
-        last=search_tweets(last,"#littlejporttalbot")
+        last=search_tweets(last,"Port Talbot")
         save_latest(last)
 #        print last
         time.sleep(20)
